c6b54bffa01e16144594107a4d90f324d14f606a
daniwidodo <dani.widodo87@gmail.com>
2018-01-25 10:19:13 +0800

    Initial Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AndroidExoplayer } from '@ionic-native/android-exoplayer';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  constructor(public navCtrl: NavController, private androidExoPlayer: AndroidExoplayer) 
+    { }
+    this.androidExoPlayer.show({url: 'https://www.youtube.com/watch?v=8GX9krUThE8'});
 }
